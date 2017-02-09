d346f01814a805ee554d609fafec6e7405dd9823
apel-apel <530328032@qq.com>
2017-02-09 16:27:12 +0800

    c

--- gaia-container-boot/src/main/java/org/apel/gaia/container/boot/customize/AvoidDuplicateSubmitHandler.java
+++ gaia-container-boot/src/main/java/org/apel/gaia/container/boot/customize/AvoidDuplicateSubmitHandler.java
@@ -1,13 +1,8 @@
 package org.apel.gaia.container.boot.customize;
 
-import java.util.UUID;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apel.gaia.container.boot.customize.annotation.AvoidDuplicateSubmit;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -20,51 +15,16 @@ import org.springframework.web.servlet.ModelAndView;
  */
 public class AvoidDuplicateSubmitHandler implements HandlerInterceptor{
 
-	private final Logger LOG = LoggerFactory.getLogger(AvoidDuplicateSubmitHandler.class);
-	
-	private final static String AVOID_DUP_SUBMIT_TOKEN = "avoidDupToken";
-
+	@SuppressWarnings("unused")
 	@Override
 	public boolean preHandle(HttpServletRequest request,
 			HttpServletResponse response, Object handler) throws Exception {
 		//controller处理之前
 		if(handler instanceof HandlerMethod){
 			HandlerMethod handlerMethod = (HandlerMethod)handler;
-			AvoidDuplicateSubmit avoidDuplicateSubmitAnnotation = handlerMethod.getMethodAnnotation(AvoidDuplicateSubmit.class);
-			if(avoidDuplicateSubmitAnnotation != null){
-				if(avoidDuplicateSubmitAnnotation.putToken()){
-					request.getSession().setAttribute(AVOID_DUP_SUBMIT_TOKEN, UUID.randomUUID().toString());
-				}
-				synchronized (getClass()) {
-					if(avoidDuplicateSubmitAnnotation.checkToken()){
-						if(isRepeatSubmit(request)){
-							 if (isRepeatSubmit(request)) {
-			                        LOG.warn("方法 = [" + handlerMethod.getMethod().getName() + "] 发生重复提交");
-			                        return false;
-			                    }
-			                    request.getSession().removeAttribute(AVOID_DUP_SUBMIT_TOKEN);
-						}
-					}
-				}
-			}
 		}
 		return true;
 	}
-	
-	private boolean isRepeatSubmit(HttpServletRequest request) {
-        String serverToken = (String) request.getSession().getAttribute(AVOID_DUP_SUBMIT_TOKEN);
-        if (serverToken == null) {
-            return true;
-        }
-        String clinetToken = request.getParameter(AVOID_DUP_SUBMIT_TOKEN);
-        if (clinetToken == null) {
-            return true;
-        }
-        if (!serverToken.equals(clinetToken)) {
-            return true;
-        }
-        return false;
-    }
 
 
 	@Override
