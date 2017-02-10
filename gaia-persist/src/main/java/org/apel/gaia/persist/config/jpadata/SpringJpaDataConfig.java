49dcbb44a0143e697045a0dde72e5034816b624c
apel-apel <530328032@qq.com>
2017-02-10 09:15:04 +0800

    c

--- gaia-persist/src/main/java/org/apel/gaia/persist/config/jpadata/SpringJpaDataConfig.java
+++ gaia-persist/src/main/java/org/apel/gaia/persist/config/jpadata/SpringJpaDataConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EnableJpaRepositories(basePackages = "org.apel.**.dao", 
+@EnableJpaRepositories(basePackages = {"org.**.dao", "com.**.dao"}, 
 							repositoryBaseClass = CommonRepositoryImpl.class)
 public class SpringJpaDataConfig {
 	
