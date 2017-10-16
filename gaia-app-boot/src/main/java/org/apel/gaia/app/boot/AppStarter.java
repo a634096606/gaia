c1237658687f404e77767f01d0face5821c9d539
apel-apel <530328032@qq.com>
2017-10-16 17:07:20 +0800

    c

--- gaia-app-boot/src/main/java/org/apel/gaia/app/boot/AppStarter.java
+++ gaia-app-boot/src/main/java/org/apel/gaia/app/boot/AppStarter.java
@@ -1,9 +1,14 @@
 package org.apel.gaia.app.boot;
 
+import java.util.concurrent.CountDownLatch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.MultipartAutoConfiguration;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ImportResource;
 
 /**
@@ -15,8 +20,30 @@ import org.springframework.context.annotation.ImportResource;
 @ImportResource("classpath*:META-INF/spring/module-*.xml")
 public class AppStarter {
 	
+	private static final Logger LOG = LoggerFactory.getLogger(AppStarter.class);
+	
 	public static ApplicationContext start(String... args){
-		 return SpringApplication.run(AppStarter.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(AppStarter.class, args);
+		LOG.info("应用服务已成功启动!");
+		return context;
+	}
+	
+	public static void startByBlocking(ContextStartedCallback contextStartedCallback, String... args) throws Exception{
+		CountDownLatch cdl = new CountDownLatch(1);
+		ConfigurableApplicationContext context = SpringApplication.run(AppStarter.class, args);
+		if (contextStartedCallback != null) {
+			contextStartedCallback.execute(context);
+		}
+		LOG.info("应用服务已成功启动!");
+		cdl.await();
+	}
+	
+	public static void startByBlocking(String... args) throws Exception{
+		startByBlocking(null, args);
+	}
+	
+	public interface ContextStartedCallback{
+		void execute(ConfigurableApplicationContext context);
 	}
 	
 }
--- gaia-jar-build/src/main/java/main/MainClass.java
+++ gaia-jar-build/src/main/java/main/MainClass.java
@@ -1,36 +1,35 @@
 package main;
 
-import java.util.TimeZone;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.MultipartAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ImportResource;
 
-import org.apel.gaia.container.boot.SimplePlatformStarter;
-
-
-
-
-
-
-
-
+@EnableAutoConfiguration(exclude = { MultipartAutoConfiguration.class })
+@ImportResource("classpath*:META-INF/spring/module-*.xml")
+@EnableEurekaClient
 public class MainClass {
-	
-	//spring boot启动
-	public static void main(String[] args) throws Exception{
-		TimeZone.setDefault(TimeZone.getTimeZone("GMT+8"));
-		SimplePlatformStarter.start(args);
+
+	// spring boot启动
+	// public static void main(String[] args) throws Exception{
+	// TimeZone.setDefault(TimeZone.getTimeZone("GMT+8"));
+	// SimplePlatformStarter.start(args);
+	// }
+
+	// //dubbo 服务启动
+	// public static void main(String[] args) throws Exception {
+	// CountDownLatch cdl = new CountDownLatch(1);
+	// ClassPathXmlApplicationContext context = new
+	// ClassPathXmlApplicationContext("classpath*:META-INF/spring/module-*.xml");
+	// context.start();
+	// System.out.println("************服务启动完成");
+	// cdl.await();
+	// context.close();
+	// }
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainClass.class, args);
 	}
-	
-//	//dubbo 服务启动
-//	public static void main(String[] args) throws Exception {
-//		CountDownLatch cdl = new CountDownLatch(1);
-//		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath*:META-INF/spring/module-*.xml");
-//		context.start();
-//		System.out.println("************服务启动完成");
-//		cdl.await();
-//		context.close();
-//	}
-	
-	
-	
-	
-	
+
 }
